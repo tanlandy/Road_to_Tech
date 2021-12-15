f2a87791d912b8eaeea5dde45362640bd64f060f
Kunlin <tanlandy6@gmail.com>
2021-12-14 17:10:50 -0800

    Learned inheritancence

---
+++ JAVA_Basic/Inheritancence.java
@@ -0,0 +1,4 @@
+// Now Triangle has inherited traits from Shape, meaning it copied over class members from Shape. 
+// When we use inheritance to extend a subclass from a superclass, we create an “is-a” relationship 
+// from the subclass to the superclass. For example, an object of Triangle is a member of the Shape class;
+//  however, an object of Shape is not necessarily an object of Triangle.
